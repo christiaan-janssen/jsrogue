73580f01db8ba21dbdc73582cde6bde416b84784
christiaan-janssen <christiaanjanssen@outlook.com>
2021-10-01 09:24:52 +0200

    Make the @ move

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-import { Display } from "rot-js";
+import { Display, KEYS } from "rot-js";
 
 const GAME_WIDTH = 50;
 const GAME_HEIGHT = 30;
 
-var display = new Display({ width: GAME_WIDTH, height: GAME_HEIGHT });
+let playerX = 12;
+let playerY = 10;
 
-display.draw(12, 10, "@", "white");
+let display = new Display({ width: GAME_WIDTH, height: GAME_HEIGHT });
 
+function drawGame() {
+    display.clear();
+    display.draw(playerX, playerY, "@", "white");
+}
+
+drawGame();
+
+function handleKeys(e) {
+    let key = e.code;
+    if (key === 'ArrowRight') {
+        playerX += 1;
+    }
+    else if (key === 'ArrowLeft') {
+        playerX -= 1;
+    }
+    else if (key === 'ArrowUp') {
+        playerY -= 1;
+    }
+    else if (key === 'ArrowDown') {
+        playerY += 1;
+    }
+    drawGame();
+}
+
+
+window.addEventListener("keyup", (e) => handleKeys(e));
 document.body.appendChild(display.getContainer());
