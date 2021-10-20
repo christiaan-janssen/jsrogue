cfcfb7fc0b5e687d8791ca2d6a5777b9d1b6cb0e
christiaan-janssen <christiaanjanssen@outlook.com>
2021-10-20 07:07:42 +0200

    Add more keys and diagonal movement

--- src/input_handlers.js
+++ src/input_handlers.js
@@ -1,23 +1,45 @@
 import { BumpAction } from './actions'
 
 export class EventHandler {
-    constructor() { }
+    constructor() {
+        this.keys = {
+            'ArrowRight': [1, 0],
+            'Numpad6': [1, 0],
+            'KeyL': [1, 0],
+
+            'ArrowLeft': [-1, 0],
+            'Numpad4': [-1, 0],
+            'KeyH': [-1, 0],
+
+            'ArrowUp': [0, -1],
+            'Numpad8': [0, -1],
+            'KeyK': [0, -1],
+
+            'ArrowDown': [0, 1],
+            'Numpad2': [0, 1],
+            'KeyJ': [0, 1],
+
+            'Numpad7': [-1, -1],
+            'KeyY': [-1, -1],
+
+            'Numpad9': [1, -1],
+            'KeyU': [1, -1],
+
+            'Numpad3': [1, 1],
+            'KeyN': [1, 1],
+
+            'Numpad1': [-1, 1],
+            'KeyB': [-1, 1],
+        }
+    }
 
     handleKeys(ev) {
         let key = ev.code;
         let action;
 
-        if (key === 'ArrowRight') {
-            action = new BumpAction(1, 0);
-        }
-        else if (key === 'ArrowLeft') {
-            action = new BumpAction(-1, 0);
-        }
-        else if (key === 'ArrowUp') {
-            action = new BumpAction(0, -1);
-        }
-        else if (key === 'ArrowDown') {
-            action = new BumpAction(0, 1);
+        if (this.keys.hasOwnProperty(key)) {
+            let [x, y] = this.keys[key];
+            action = new BumpAction(x, y);
         }
 
         return action;
